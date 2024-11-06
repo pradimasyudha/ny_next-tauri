5b1b81b561eca2f6d819b807c2c9368928ffb918
Dimas Yudha Pratama <pradimasyudha@gmail.com>
2024-11-07 01:33:21 +0700

    feat: init service side project with `Tauri`

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: "http://localhost:3000",
 };
 
 export default nextConfig;
